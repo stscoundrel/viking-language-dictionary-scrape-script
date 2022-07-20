19c11bb549103b2c95932fe841f9dffebc5dfc80
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2022-07-20 20:10:45 +0300

    Script: parse &amp;s

--- script.ts
+++ script.ts
@@ -4,7 +4,7 @@ document.querySelectorAll('.has-small-font-size').forEach(wordLine => {
     const headword = wordLine.querySelectorAll('strong')[0].innerHTML
     const word = {
         headword: headword.replace(/<[^>]*>?/gm, ''),
-        definition: wordLine.innerHTML.replace(headword, '').replace(/<[^>]*>?/gm, '').replace('', '').replace('&lt;', '<').replace('&gt;', '>').trim(),
+        definition: wordLine.innerHTML.replace(headword, '').replace(/<[^>]*>?/gm, '').replace('', '').replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&').trim(),
         isCommon: wordLine.innerHTML.includes('')
     }
 
