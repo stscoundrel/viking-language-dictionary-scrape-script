824636528a56e76e43a2b113b84dddac25d0606b
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2022-08-03 14:45:46 +0300

    Move misplaced genders from headword to definition
    Closes #2

--- script.ts
+++ script.ts
@@ -1,19 +1,49 @@
 const words = [];
 
+const MALFORMATTED_ENDS = [' f', ' m', ' n'];
+
+const parseHeadWord = (headword) => {
+  let parsed = headword
+    .replace(/<[^>]*>?/gm, '')
+    .trim();
+
+  const headwordEnd = parsed.slice(-2);
+  if (MALFORMATTED_ENDS.includes(headwordEnd)) {
+    MALFORMATTED_ENDS.forEach((malformatted) => {
+      parsed = parsed.replace(malformatted, '');
+    });
+  }
+
+  return parsed;
+};
+
+const parseDefinition = (headword, wordLine) => {
+  let parsed = wordLine.innerHTML
+    .replace(headword, '')
+    .replace(/<[^>]*>?/gm, '')
+    .replace('', '')
+    .replace('&lt;', '<')
+    .replace('&gt;', '>')
+    .replace('&amp;', '&')
+    .trim();
+
+  const parsedHeadWord = headword.replace(/<[^>]*>?/gm, '').trim();
+  const headwordEnd = parsedHeadWord.slice(-2);
+
+  MALFORMATTED_ENDS.forEach((malformatted) => {
+    if (headwordEnd === malformatted) {
+      parsed = `${malformatted} ${parsed}`;
+    }
+  });
+
+  return parsed;
+};
+
 document.querySelectorAll('.has-small-font-size').forEach((wordLine) => {
   const headword = wordLine.querySelectorAll('strong')[0].innerHTML;
   const word = {
-    headword: headword
-      .replace(/<[^>]*>?/gm, '')
-      .trim(),
-    definition: wordLine.innerHTML
-      .replace(headword, '')
-      .replace(/<[^>]*>?/gm, '')
-      .replace('', '')
-      .replace('&lt;', '<')
-      .replace('&gt;', '>')
-      .replace('&amp;', '&')
-      .trim(),
+    headword: parseHeadWord(headword),
+    definition: parseDefinition(headword, wordLine),
     isCommon: wordLine.innerHTML.includes(''),
   };
 
