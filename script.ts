15ac7e65ba3e274a4f4239ff878a42b2422c4cbb
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2022-08-03 14:23:43 +0300

    Trim spaces in headwords
    Fixes #1

--- script.ts
+++ script.ts
@@ -1,14 +1,23 @@
-const words = []
+const words = [];
 
-document.querySelectorAll('.has-small-font-size').forEach(wordLine => {
-    const headword = wordLine.querySelectorAll('strong')[0].innerHTML
-    const word = {
-        headword: headword.replace(/<[^>]*>?/gm, ''),
-        definition: wordLine.innerHTML.replace(headword, '').replace(/<[^>]*>?/gm, '').replace('', '').replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&').trim(),
-        isCommon: wordLine.innerHTML.includes('')
-    }
+document.querySelectorAll('.has-small-font-size').forEach((wordLine) => {
+  const headword = wordLine.querySelectorAll('strong')[0].innerHTML;
+  const word = {
+    headword: headword
+      .replace(/<[^>]*>?/gm, '')
+      .trim(),
+    definition: wordLine.innerHTML
+      .replace(headword, '')
+      .replace(/<[^>]*>?/gm, '')
+      .replace('', '')
+      .replace('&lt;', '<')
+      .replace('&gt;', '>')
+      .replace('&amp;', '&')
+      .trim(),
+    isCommon: wordLine.innerHTML.includes(''),
+  };
 
-    words.push(word)
-})
+  words.push(word);
+});
 
-console.log(words)
+console.log(words);
